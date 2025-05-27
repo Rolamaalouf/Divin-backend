d6170bb3a7ac1865b72d21559bd93e90a9d970ec
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-05-27 11:04:16 +0300

    Fix CORS preflight handling for frontend

--- server.js
+++ server.js
@@ -21,18 +21,17 @@ const allowedOrigins = [
   "https://divin-frontend.vercel.app",
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.options('*', cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 
 // Middlewares
 app.use(express.json());
